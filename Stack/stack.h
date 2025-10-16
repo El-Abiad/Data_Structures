8f304a3ebc880b8be2782209811bfc63204e6d16
El-Abiad <moelabiad26@gmail.com>
2025-10-16 12:38:42 -0700

    Stack Implementation

---
+++ Stack/stack.h
@@ -0,0 +1,17 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define MAX_SIZE 100
+#define TYPE int
+
+typedef struct{
+    TYPE arr[100];
+} Stack;
+
+// methods
+void createStack(Stack *s);
+int isEmpty(Stack *s);
+int isFull(Stack *s);
+void push(Stack *s, TYPE x);
+TYPE pop(Stack *s);
+#endif
